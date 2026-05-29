7579fe1a6bb5d722b13ae7bcdc512f6cc00adcc8
gabe-rbo <gabrielribeiro@dcc.ufmg.br>
2026-05-29 13:49:32 -0300

    versioning fix

--- pysurgery/__init__.py
+++ pysurgery/__init__.py
@@ -315,7 +315,7 @@ from .auto_surgery import (
 
 from . import integrations
 
-__version__ = "2.0.3"
+__version__ = "2.0.4"
                                 
 def __getattr__(name):
     if name == "JuliaBridge":
