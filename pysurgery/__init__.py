2bd574520de84edbf6b70247decf97ac0df975c3
gabe-rbo <gabrielribeiro@dcc.ufmg.br>
2026-05-29 13:21:50 -0300

    chore: restore auto-versioning workflow

--- pysurgery/__init__.py
+++ pysurgery/__init__.py
@@ -315,7 +315,7 @@ from .auto_surgery import (
 
 from . import integrations
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
                                 
 def __getattr__(name):
     if name == "JuliaBridge":
