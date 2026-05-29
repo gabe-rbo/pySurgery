ee21a45923d54032a5eca8339167275a0a795b46
gabe-rbo <gabrielribeiro@dcc.ufmg.br>
2026-05-29 13:17:34 -0300

    fix jax_bridge bug

--- pysurgery/__init__.py
+++ pysurgery/__init__.py
@@ -315,7 +315,7 @@ from .auto_surgery import (
 
 from . import integrations
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
                                 
 def __getattr__(name):
     if name == "JuliaBridge":
